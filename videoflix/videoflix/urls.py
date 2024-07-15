5228bccfd3ea34aa3d82f994aebe30dfc64b875c
Landvoigt <tim.voigt@ik.me>
2024-07-15 21:30:56 +0200

    updated urls

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -23,45 +23,54 @@ from django.conf.urls.static import static
 
 from rest_framework.routers import DefaultRouter
 
+from verification_token.views import UserVerifyEmailView
+from .views import ContactView
+
 from users.views import UserLoginView, UserCreateView, UserResetPasswordView, ValidateResetTokenView, user_update_username
 from profiles.views import ProfileViewSet
-from verification_token.views import UserVerifyEmailView
-
-from .views import ContactView
-from videostore.views import get_video_url,get_poster_urls
-from videostore.views import get_all_video_urls,  get_all_videos
-from videostore.views import gcs_video_text, check_video_data
+from videostore.views import get_video_url,get_poster_urls, get_all_video_urls, get_all_videos, gcs_video_text, check_video_data
 
 
-def home_view(request):
+def home_view():
     return HttpResponse("Welcome to the home page!")
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
-urlpatterns = [
 
+urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('register/', UserCreateView.as_view(), name='register'),
-    path('contact/', ContactView.as_view(), name='contact'),
-
-    path('api/verify-email/<uuid:token>/', UserVerifyEmailView.as_view(), name='verify_email'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path('api/password_reset/validate/', ValidateResetTokenView.as_view(), name='password_reset_validate'),
-    path('api/password_reset/confirm/<str:uidb64>/<str:token>/', UserResetPasswordView.as_view(), name='password_reset_confirm'),
-    path('api/update_username/', user_update_username, name='update_username'),
-
-    path('', include(router.urls)), 
-
     path('django-rq/', include('django_rq.urls')),
-    
+
+    # move to api_patterns later
     path('get-video-url/', get_video_url, name='get_video_url'),
     path('get_poster_urls/', get_poster_urls, name='get_poster_urls'),
     path('get-all-video-urls/', get_all_video_urls, name='get_all_video_urls'),
     path('api/videos/', get_all_videos, name='get_all_videos'),
-    path('check-video-data/', check_video_data, name='check-video-data'),
+    path('check-video-data/', check_video_data, name='check_video_data'),
     path('gcs-data/', gcs_video_text, name='gcs_data'),
-  
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+api_patterns = [
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('update_username/', user_update_username, name='update_username'),
+
+    path('verify-email/<uuid:token>/', UserVerifyEmailView.as_view(), name='verify_email'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('password_reset/validate/', ValidateResetTokenView.as_view(), name='password_reset_validate'),
+    path('password_reset/confirm/<str:uidb64>/<str:token>/', UserResetPasswordView.as_view(), name='password_reset_confirm'),
+    
+    # path('get-video-url/', get_video_url, name='get_video_url'),
+    # path('get_poster_urls/', get_poster_urls, name='get_poster_urls'),
+    # path('get-all-video-urls/', get_all_video_urls, name='get_all_video_urls'),
+    # path('videos/', get_all_videos, name='get_all_videos'),
+    # path('check-video-data/', check_video_data, name='check_video_data'),
+    # path('gcs-data/', gcs_video_text, name='gcs_data'),
+
+    path('', include(router.urls)),
+]
+
+urlpatterns += [path('api/', include(api_patterns))]
--- videoflix/videostore/models.py
+++ videoflix/videostore/models.py
@@ -1,28 +1,28 @@
-from django.db import models
-from datetime import date
 import os
+
+from datetime import date
+from django.db import models
+
 
 class Video(models.Model):
     created_at = models.DateField(default=date.today)
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=500)
-    video_file = models.FileField(upload_to='videos', blank=True, null=True)
-    hls_playlist = models.CharField(max_length=200, blank=True, null=True) 
-    
+    video_file = models.FileField(upload_to="videos", blank=True, null=True)
+    hls_playlist = models.CharField(max_length=200, blank=True, null=True)
+    # add later
+    # category = models.CharField(max_length=50, blank=True, null=True) 
+
     def save(self, *args, **kwargs):
         if self.video_file and not self.hls_playlist:
             video_name = os.path.splitext(os.path.basename(self.video_file.name))[0]
-            gcs_url = f'https://storage.googleapis.com/videoflix-videos/hls/{video_name}/master.m3u8'
+            gcs_url = f"https://storage.googleapis.com/videoflix-videos/hls/{video_name}/master.m3u8"
             self.hls_playlist = gcs_url
-            
+
         if self.video_file:
             self.video_file.name = os.path.basename(self.video_file.name)
-        
-        super().save(*args, **kwargs) 
-    
+
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
-
-
-
-
