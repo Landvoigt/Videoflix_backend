1b8c324a123990eb8012dce2f62cbae6ead935a1
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2024-10-09 21:02:57 +0200

    .env

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -16,11 +16,14 @@ import os
 from google.oauth2 import service_account
 from django_redis.cache import RedisCache
 
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+#environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
